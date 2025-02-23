cb107d4c1d9972e1ca1d09ac8fd5d09e20e32a76
mahmudul1024 <mahmudul.info.bd@gmail.com>
2025-02-23 17:34:24 +0100

    slider swiper added

---
+++ src/Home/Category.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
+import sl1 from "../assets/home/slide1.jpg";
+import sl2 from "../assets/home/slide2.jpg";
+import sl3 from "../assets/home/slide3.jpg";
+
+const Category = () => {
+  return (
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <img src={sl1} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={sl2} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={sl3} alt="" />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Category;

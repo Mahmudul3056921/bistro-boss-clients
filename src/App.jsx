8c57775d64d12f49eee421ff0debccd5d3c4e496
mahmudul1024 <mahmudul.info.bd@gmail.com>
2025-02-23 16:23:34 +0100

    navbar and footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
   <>
-  <button className="btn btn-active">Default</button>
-<button className="btn btn-active btn-neutral">Neutral</button>
-<button className="btn btn-active btn-primary">Primary</button>
+  
+
   </>
   )
 }

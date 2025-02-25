cf883e59cf203f63d5c3bcf5a09007c46b8f9307
mahmudul1024 <mahmudul.info.bd@gmail.com>
2025-02-25 21:59:32 +0100

    parallax has been used finally

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,10 +3,14 @@ import Category from "./Category";
 import PopularMenu from "../Shared/PopularMenu/PopularMenu";
 import FeaturedItem from "./FeaturedItem/FeaturedItem";
 import Testimonials from "./Testimonial/Testimonials";
+import { Helmet } from "react-helmet-async";
 
 export const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Its Home page</title>
+      </Helmet>
       <Banner></Banner>
       <Category></Category>
       <PopularMenu></PopularMenu>
--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { Helmet } from "react-helmet-async";
+import Cover from "../Pages/Cover/Cover";
+import coverImage from "../assets/menu/salad-bg.jpg";
+import PizzaImage from "../assets/menu/pizza-bg.jpg";
+import PopularMenu from "../Shared/PopularMenu/PopularMenu";
 const Menu = () => {
   return (
     <div>
-      <h1>Menu</h1>
+      <Helmet>
+        <title>Menu page</title>
+      </Helmet>
+      <Cover coverImage={coverImage} title="Salad"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover coverImage={PizzaImage} title="Pizza"></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
